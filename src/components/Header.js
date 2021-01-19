eba5599f2533f381887b221ec76b0f63013665a3
Manot Luijiu <manot.luijiu@gmail.com>
2021-01-19 16:46:29 +0700

    adding json-server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   const onClick = () => {
-    console.log('onClick');
+    onAdd();
   };
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onClick}
+      />
     </header>
   );
 };
