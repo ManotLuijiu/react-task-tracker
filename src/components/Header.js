33c62d2101dd0ddb5bc82181529420bb4ab3bde5
Manot Luijiu <manot.luijiu@gmail.com>
2021-01-19 18:01:40 +0700

    adding react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   const onClick = () => {
     onAdd();
   };
@@ -9,11 +12,13 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? 'red' : 'green'}
-        text={showAdd ? 'Close' : 'Add'}
-        onClick={onClick}
-      />
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onClick}
+        />
+      )}
     </header>
   );
 };
